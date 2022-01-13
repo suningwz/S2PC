44928c90643bf73b15d542422befe4349edb9e3d
AlphaConseils <alpha@alphamada.com>
2022-01-13 09:44:24 +0100

    feat: employe, contact, achat, maintenance

--- s2pc_maintenance/models/maintenance_equipment_inherited.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-
-class MaintenanceEquipmentInherited(models.Model):
-    _inherit = 'maintenance.equipment'
-    etalon = fields.Char(string="Etalon / Standard", index=1)

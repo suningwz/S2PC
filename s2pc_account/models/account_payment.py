64ea671e403318f73cb16faf9f76523b77553663
MagnusCalsen <herivahatra3noob@gmail.com>
2022-01-18 23:01:37 +0300

    [IMPL] Double validation on payment

---
+++ s2pc_account/models/account_payment.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class AccountPayment(models.Model):
+	_inherit = 'account.payment'
+
+	need_validation = fields.Boolean('Need validation', default=False, tracking=True)
+
+	def action_need_validation(self):
+		for rec in self:
+			rec.need_validation = True
+
+	def action_post(self):
+		res = super(AccountPayment, self).action_post()
+		for rec in self:
+			rec.need_validation = False
+		return res

8322591ac910e4d048e8c25c034d217001821eaa
MagnusCalsen <herivahatra3noob@gmail.com>
2022-01-25 16:26:51 +0300

    [FIX] Add deleted codes

--- s2pc_employe/models/hr_employee.py
+++ s2pc_employe/models/hr_employee.py
@@ -5,4 +5,6 @@ from odoo import models, fields, api
 
 class HrEmployee(models.Model):
     _inherit = 'hr.employee'
-    cnaps_number = fields.Char('Numéro CNAPS', index=1)
+
+    cnaps_number = fields.Char('Numero CNAPS', index=1)
+    team_employee_ids = fields.Many2many('hr.employee', 'hr_employee_team_rel', 'manager_id', 'employee_id', 'Team')

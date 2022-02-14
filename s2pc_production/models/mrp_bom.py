bbee7fb5a8379619830453e5f9a8bd9f5927b95e
AlphaConseils <alpha@alphamada.com>
2022-02-14 17:05:26 +0100

    Production progress

--- s2pc_production/models/mrp_bom.py
+++ s2pc_production/models/mrp_bom.py
@@ -4,9 +4,8 @@ from odoo import fields, models, api
 class ModelName(models.Model):
     _inherit = 'mrp.bom.line'
     _description = 'Use to add a section and note on bom line'
-    product_id = fields.Many2one('product.product', 'Component', required=False, check_company=True)
 
     display_type = fields.Selection([
         ('line_section', "Section"),
         ('line_note', "Note")], default=False, help="Technical field for UX purpose.")
-    name = fields.Text(string='Description', required=True)
+    name = fields.Text(string='Description')
--- s2pc_production/models/mrp_production.py
+++ s2pc_production/models/mrp_production.py
@@ -6,4 +6,62 @@ class ModelName(models.Model):
     _inherit = 'mrp.production'
     mrp_team = fields.Many2one('mrp.team', string="Equipe de production")
     current_date = fields.Date(string="To days date", default=datetime.now())
-    date_planned_start_related = fields.Datetime(string="Infos date prevue", related="date_planned_start", readonly=True)
+    date_planned_start_related = fields.Datetime(string="Infos date prevue", related="date_planned_start",
+                                                 readonly=True)
+    workorder_ids_state = fields.Char(string="Opération en cours", compute="get_state_workoderids")
+    consumption_warning_ids = fields.Many2many('mrp.consumption.warning')
+
+    @api.depends('workorder_ids')
+    def get_state_workoderids(self):
+        for rec in self.workorder_ids:
+            if rec.state == "progress":
+                self.workorder_ids_state = rec.name
+            else:
+                self.workorder_ids_state = ""
+
+    def get_workorder(self):
+        record = self.env['mrp.consumption.record']
+        print('rec avant')
+        for rec in record:
+            print(rec)
+        return True
+
+    def get_component(self):
+        component_adjustement = self.env['mrp.consumption.warning.line']
+        for rec in component_adjustement:
+            print(rec)
+        return True
+
+    @api.onchange('move_raw_ids')
+    def onchange_move(self):
+        print("move_raw_ids on change")
+        for rec in self.move_raw_ids:
+            print(rec.name)
+
+    def _action_generate_consumption_wizard(self, consumption_issues):
+        ctx = self.env.context.copy()
+        lines = []
+        for order, product_id, consumed_qty, expected_qty in consumption_issues:
+            print("mandalo append")
+            lines.append((0, 0, {
+                'mrp_production_id': order.id,
+                'product_id': product_id.id,
+                'consumption': order.consumption,
+                'product_uom_id': product_id.uom_id.id,
+                'product_consumed_qty_uom': consumed_qty,
+                'product_expected_qty_uom': expected_qty
+            }))
+            print("eto ao zao")
+            self.env['mrp.consumption.record'].create({
+                'mrp_production_id': order.id,
+                'product_id': product_id.id,
+                'consumption': order.consumption,
+                'product_uom_id': product_id.uom_id.id,
+                'product_consumed_qty_uom': consumed_qty,
+                'product_expected_qty_uom': expected_qty
+            })
+            print("eto ah zao vita create")
+        ctx.update({'default_mrp_production_ids': self.ids, 'default_mrp_consumption_warning_line_ids': lines})
+        action = self.env["ir.actions.actions"]._for_xml_id("mrp.action_mrp_consumption_warning")
+        action['context'] = ctx
+        return action

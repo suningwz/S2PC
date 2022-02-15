81f7972fa1ad9f1bc56d8cf4d52ebbe39eb78cbf
AlphaConseils <alpha@alphamada.com>
2022-02-15 17:40:08 +0100

    FIX(s2pc): Test purpose

--- s2pc_production/models/mrp_bom.py
+++ s2pc_production/models/mrp_bom.py
@@ -9,3 +9,12 @@ class ModelName(models.Model):
         ('line_section', "Section"),
         ('line_note', "Note")], default=False, help="Technical field for UX purpose.")
     name = fields.Text(string='Description')
+    product_id = fields.Many2one('product.product', 'Component', required=True, check_company=False)
+
+    # @api.model_create_multi
+    # def create(self, vals_list):
+    #     for values in vals_list:
+    #         if values.get('display_type', self.default_get(['display_type'])['display_type']):
+    #             values.update(product_id=False)
+    #     lines = super().create(vals_list)
+    #     return lines

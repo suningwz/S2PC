a57b5267de05a589e59989d942109f0c7ff286f6
AlphaConseils <alpha@alphamada.com>
2022-02-16 12:36:35 +0100

    FEAT(vente, stock, account) : hide report order on sale module, hide report preparation with bar code on stock, and change action_post() to button_validate  account model

--- s2pc_vente/__manifest__.py
+++ s2pc_vente/__manifest__.py
@@ -25,7 +25,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
+        'views/hide_report.xml',
         'views/templates.xml',
     ],
     # only loaded in demonstration mode

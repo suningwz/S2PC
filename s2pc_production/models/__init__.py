2f3281cdfc81e175b6c8b49e24f4bceb9d784b2b
AlphaConseils <alpha@alphamada.com>
2022-02-25 17:03:35 +0100

    fix conflit mrp.production to mrp_production_val

--- s2pc_production/models/__init__.py
+++ s2pc_production/models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import mrp_workorder
 from . import mrp_team
-from . import mrp_production
+from . import mrp_production_val
 from . import mrp_bom
 from . import MrpConsumptionWarningTransien
 from . import stock_move

d7e9ec1740040b8e8afd2633a77fd1f797863d99
AlphaConseils <alpha@alphamada.com>
2022-03-14 15:09:15 +0100

    FEAT[s2pc_employe, s2pc_production] : Add lang Malagasy, another

--- s2pc_employe/models/hr_employee.py
+++ s2pc_employe/models/hr_employee.py
@@ -8,8 +8,18 @@ class HrEmployee(models.Model):
 
     cnaps_number = fields.Char('Numero CNAPS', index=1)
     team_employee_ids = fields.Many2many('hr.employee', 'hr_employee_team_rel', 'manager_id', 'employee_id', 'Team')
+    langue = fields.Selection(
+        [('english', 'Anglais'),
+         ('french', 'Français'),
+         ('malgace', 'Malagasy')],
+        'Langue')
 
 
 class HrEmployeePublic(models.Model):
     _inherit = 'hr.employee.public'
     cnaps_number = fields.Char(readonly=True)
+    langue = fields.Selection(
+        [('english', 'Anglais'),
+         ('french', 'Français'),
+         ('malgace', 'Malagasy')],
+        'Langue')

9699fdb244ffbae1df9b3ac0b75eabcebecd3f3c
MagnusCalsen <herivahatra3noob@gmail.com>
2022-03-17 17:50:19 +0300

    [FIX] Duplicate on bom line and mrp production

--- s2pc_production/models/mrp_bom.py
+++ s2pc_production/models/mrp_bom.py
@@ -27,13 +27,34 @@
 # -*- coding: utf-8 -*-
 
 from odoo import fields, models, api
-
+from odoo.tools.float_utils import float_compare
 
 class MrpBomLine(models.Model):
-    _inherit = 'mrp.bom.line'
+	_inherit = 'mrp.bom.line'
 
-    restricted_operation_id = fields.Many2one('mrp.routing.workcenter')
+	restricted_operation_id = fields.Many2one('mrp.routing.workcenter', copy=False)
 
-    @api.model
-    def get_operation(self):
-        return self.bom_id.operation_ids.read(['id', 'name'])
+	@api.model
+	def get_operation(self):
+		return self.bom_id.operation_ids.read(['id', 'name'])
+
+
+class MrpBom(models.Model):
+	_inherit = 'mrp.bom'
+
+	def copy(self, default=None):
+		res = super(MrpBom, self).copy(default)
+		for bom_line in self.bom_line_ids:
+			if bom_line.restricted_operation_id:
+				operation = self.env['mrp.routing.workcenter'].search([('name', '=', bom_line.restricted_operation_id.name),
+				                                                       ('id', 'in', res.operation_ids.ids)], limit=1)
+				new_bom_line = fields.first(res.bom_line_ids.filtered(
+					lambda l: l.product_id == bom_line.product_id
+					          and float_compare(l.product_qty, bom_line.product_qty, precision_rounding=l.product_uom_id.rounding) == 0
+					          and l.product_uom_id == bom_line.product_uom_id
+							  and not l.restricted_operation_id
+
+				))
+				if operation and new_bom_line:
+					new_bom_line.restricted_operation_id = operation.id
+		return res
